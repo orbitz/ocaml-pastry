f6535d0520e9dd03d39307a3aefabbcb242a7c06
orbitz <orbitz@gmail.com>
2013-12-04 18:06:29 +0100

    Add a closest API

--- lib/pastry/node.ml
+++ lib/pastry/node.ml
@@ -1,3 +1,5 @@
+open Core.Std
+
 type 'a t = { a : 'a
 	    ; k : Key.t
 	    ; d : int
@@ -9,6 +11,22 @@ let create ~distance ~k a =
 let distance t = t.d
 
 let key t = t.k
+
+let closest k (t1, t2) =
+  match Key.closest k (t1.k, t2.k) with
+    | c when Key.compare c t1.k = 0 ->
+      t1
+    | _ ->
+      t2
+
+let find_closest k = function
+  | [] ->
+    None
+  | n::nodes ->
+    Some (List.fold_left
+	    ~f:(fun close_node n -> closest k (close_node, n))
+	    ~init:n
+	    nodes)
 
 let of_t t = t.a
 
