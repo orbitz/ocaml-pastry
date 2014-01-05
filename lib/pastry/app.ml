29b8a2a0dae3e619f2bd4129056f8d064708eedc
orbitz <orbitz@gmail.com>
2014-01-06 00:50:06 +0100

    Add extra params to announce, change formatting

--- lib/pastry/app.ml
+++ lib/pastry/app.ml
@@ -35,13 +35,43 @@ module type IO = sig
     val to_string : t -> string
   end
 
-  val announce   : t -> Endpoint.t -> (Endpoint.t Msg.Announce_resp.t, unit) Deferred.Result.t
-  val send_state : t -> Endpoint.t -> Endpoint.t Router.t -> (unit, unit) Deferred.Result.t
-  val send       : t -> Endpoint.t -> Msg.Payload.t -> (unit, unit) Deferred.Result.t
-  val ping       : t -> Endpoint.t -> (unit, unit) Deferred.Result.t
-  val distance   : t -> Endpoint.t -> (int, unit) Deferred.Result.t
-  val listen     : t -> (Endpoint.t Msg.All.t, unit) Deferred.Result.t
-  val close      : t -> unit Deferred.t
+  val announce :
+    t ->
+    Endpoint.t ->
+    announcer:Endpoint.t Node.t ->
+    routers:Endpoint.t Router.t list ->
+    (Endpoint.t Msg.Announce_resp.t, unit) Deferred.Result.t
+
+  val send_state :
+    t ->
+    Endpoint.t ->
+    Endpoint.t Router.t ->
+    (unit, unit) Deferred.Result.t
+
+  val send :
+    t ->
+    Endpoint.t ->
+    Msg.Payload.t ->
+    (unit, unit) Deferred.Result.t
+
+  val ping :
+    t ->
+    Endpoint.t ->
+    (unit, unit) Deferred.Result.t
+
+  val distance :
+    t ->
+    Endpoint.t ->
+    (int, unit) Deferred.Result.t
+
+  val listen :
+    t ->
+    (Endpoint.t Msg.All.t, unit) Deferred.Result.t
+
+  val close :
+    t ->
+    unit Deferred.t
+
 end
 
 
@@ -105,7 +135,7 @@ module Make = functor (App : APP) -> functor (Io : IO) -> struct
 
     let connect_to_cluster io endpoint router =
       let open Deferred.Result in
-      Io.announce io endpoint >>= fun resp ->
+      Io.announce io endpoint ~announcer:(Router.me router) ~routers:[] >>= fun resp ->
       let nodes = Join_protocol.router_nodes ~me:(Router.me router) resp in
       measure_distances io nodes >>= fun nodes_with_distance ->
       let router' = update_router nodes_with_distance router in
--- lib/pastry/app.mli
+++ lib/pastry/app.mli
@@ -30,13 +30,43 @@ module type IO = sig
     val to_string : t -> string
   end
 
-  val announce   : t -> Endpoint.t -> (Endpoint.t Msg.Announce_resp.t, unit) Deferred.Result.t
-  val send_state : t -> Endpoint.t -> Endpoint.t Router.t -> (unit, unit) Deferred.Result.t
-  val send       : t -> Endpoint.t -> Msg.Payload.t -> (unit, unit) Deferred.Result.t
-  val ping       : t -> Endpoint.t -> (unit, unit) Deferred.Result.t
-  val distance   : t -> Endpoint.t -> (int, unit) Deferred.Result.t
-  val listen     : t -> (Endpoint.t Msg.All.t, unit) Deferred.Result.t
-  val close      : t -> unit Deferred.t
+  val announce :
+    t ->
+    Endpoint.t ->
+    announcer:Endpoint.t Node.t ->
+    routers:Endpoint.t Router.t list ->
+    (Endpoint.t Msg.Announce_resp.t, unit) Deferred.Result.t
+
+  val send_state :
+    t ->
+    Endpoint.t ->
+    Endpoint.t Router.t ->
+    (unit, unit) Deferred.Result.t
+
+  val send :
+    t ->
+    Endpoint.t ->
+    Msg.Payload.t ->
+    (unit, unit) Deferred.Result.t
+
+  val ping :
+    t ->
+    Endpoint.t ->
+    (unit, unit) Deferred.Result.t
+
+  val distance :
+    t ->
+    Endpoint.t ->
+    (int, unit) Deferred.Result.t
+
+  val listen :
+    t ->
+    (Endpoint.t Msg.All.t, unit) Deferred.Result.t
+
+  val close :
+    t ->
+    unit Deferred.t
+
 end
 
 module Make : functor (App : APP) -> functor (Io : IO) -> sig
